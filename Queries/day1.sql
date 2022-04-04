f56e900d091431277f408588f8a667af83b6045f
Rostyslav <ctaff23@mail.ru>
2022-04-04 13:22:13 -0500

    Initial commit

---
+++ Queries/day1.sql
@@ -0,0 +1,32 @@
+-- This how you write comment in sql
+-- singile line comment llok like this
+/*
+ This is how we write multi
+ line comment
+ */
+SELECT * FROM REGIONS ;
+-- If you want to run second statement
+-- If you just need to click the query to focus the cursor and click rin
+
+SELECT * FROM COUNTRIES ;
+
+SELECT * FROM COUNTRIES ;
+
+SELECT * FROM DEPARTMENTS ;
+
+SELECT * FROM JOB_HISTORY ;
+
+SELECT * FROM JOBS ;
+
+SELECT * FROM LOCATIONS ;
+
+SELECT * FROM REGIONS ;
+
+-- RESTICTING COLUMNS IN THE RESULT
+-- SQL IS SPACE INSENSITIVE
+SELECT FIRST_NAME
+FROM EMPLOYEES ;
+
+-- More then one colums can be specified separated by comma
+SELECT FIRST_NAME,LAST_NAME
+FROM EMPLOYEES
